bdff2085cc3eb82698094305ba0025c87bf67b3c
bakedspacetime <tushyais@gmail.com>
2023-04-30 23:25:17 -0700

    add system manager declarations and implementation

---
+++ system_manager.cpp
@@ -0,0 +1,33 @@
+#include "system_manager.hpp"
+#include <iostream>
+
+#include <fstream>
+#include <sstream>
+#include <tuple>
+
+std::tuple<std::string, std::string, std::string>
+SystemManager::verifyArguments(int argc, char* argv[]) {
+    const char* databaseFilename = argv[1];
+    const char* scriptFilename = argv[2];
+    const char* logfileFilename = argv[3];
+
+    if (argc - 1 < expectedArgs) {
+        throw std::invalid_argument("Not enough arguments provided");
+
+    } else if (fileExists(scriptFilename)) {
+        return std::make_tuple(databaseFilename, scriptFilename,
+                               logfileFilename);
+    } else {
+        std::stringstream err;
+        err << "Script file at the path provided does not exist. Please "
+               "provide a valid path. ("
+            << scriptFilename << ")" << std::endl;
+        throw std::invalid_argument(err.str());
+    }
+}
+
+bool SystemManager::fileExists(const std::string& filename) {
+    std::ifstream file(filename);
+    // file.open(fname);
+    return file ? true : false;
+}
--- system_manager.hpp
+++ system_manager.hpp
@@ -1,4 +1,12 @@
+#include <string>
+#include <tuple>
 
 class SystemManager {
+   private:
+    const static int expectedArgs = 3;
+    static bool fileExists(const std::string& filename);
 
+   public:
+    static std::tuple<std::string, std::string, std::string> verifyArguments(
+        int argc, char* argv[]);
 };
