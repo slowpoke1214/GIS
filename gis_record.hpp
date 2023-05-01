079a56b2a18832a420b61ac5b6c8583a2af01014
bakedspacetime <tushyais@gmail.com>
2023-04-30 18:16:40 -0700

    Add declarations for GISRecord

--- gis_record.hpp
+++ gis_record.hpp
@@ -1,4 +1,35 @@
+#include <vector>
+#include <sstream>
+
+const char GISRecordDelimeter = '|';
+
 
 class GISRecord {
+private:
+    int feature_id;
+    std::string feature_name;
+    std::string feature_class;
+    std::string state_alpha;
+    std::string state_numeric;
+    std::string county_name;
+    std::string county_numeric;
+    std::string primary_lat_dms;
+    std::string prim_long_dms;
+    double prim_lat_dec;
+    double prim_long_dec;
+    std::string source_lat_dms;
+    std::string source_long_dms;
+    double source_lat_dec;
+    double source_long_dec;
+    int elev_in_m;
+    int elev_in_ft;
+    std::string map_name;
+    std::string date_created;
+    std::string date_edited;
 
+    std::vector<std::string> parseLine(const std::string &line);
+
+public:
+    GISRecord(const std::string &line);
+    ~GISRecord();
 };
