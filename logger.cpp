89494d479f8463600399fc1d1300bca95312f5b8
bakedspacetime <tushyais@gmail.com>
2023-04-30 21:16:18 -0700

    Implement Logger

---
+++ logger.cpp
@@ -0,0 +1,31 @@
+#include "logger.hpp"
+
+#include <fstream>
+#include <sstream>
+
+Logger::Logger() {}
+
+Logger::Logger(std::string filename) {
+    logFile = std::move(filename);
+    std::ofstream logStream(logFile);
+    logStream.close();
+}
+
+void Logger::write(std::string message) {
+    std::fstream logStream(logFile, std::ios::app);
+
+    logStream << message << std::endl;
+
+    logStream.close();
+}
+
+void Logger::write(std::string command, int commandNumber) {
+    if (commandNumber == 0)
+    {
+        write(command);
+    } else {
+        std::fstream logStream(logFile, std::ios::app);
+        logStream << "Command " << commandNumber << ": " << command << std::endl;
+        logStream.close();
+    }
+}
