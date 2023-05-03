ed7fa1ff237f8dc9ef94f5f125f7a5fe8b7adeb4
bakedspacetime <tushyais@gmail.com>
2023-05-03 16:47:28 -0700

    implement world

---
+++ world.cpp
@@ -0,0 +1,8 @@
+#include "world.hpp"
+
+#include <iostream>
+
+world::world(DMS westLong, DMS eastLong, DMS southLat, DMS northLat)
+    : Region::Region(northLat, southLat, eastLong, westLong) {
+  // std::cout << "World bottom left: " << bottomLeft.repr() << std::endl;
+}
