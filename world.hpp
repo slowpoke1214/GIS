c98ecee5c3aa01b2063fd8df9a5ab13d4a0431cd
bakedspacetime <tushyais@gmail.com>
2023-05-03 16:47:03 -0700

    define world as subclass of region

---
+++ world.hpp
@@ -0,0 +1,11 @@
+#include "gis_helpers.hpp"
+#include "world_systems/region.hpp"
+
+class world : private Region {
+ private:
+ public:
+  world(DMS westLong, DMS eastLong, DMS southLat, DMS northLat);
+};
+
+// world::world(double westLong, double eastLong, double southLat, double northLat) {}
+
