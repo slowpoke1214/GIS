1a81b2da598dab6fb8d46f74070f42859935faa7
bakedspacetime <tushyais@gmail.com>
2023-06-18 21:49:35 -0700

    implement changes to nameindex

--- database/database.cpp
+++ database/database.cpp
@@ -1,26 +1,62 @@
 #include "database.hpp"
 
 #include <fstream>
+#include <algorithm>
 #include <iostream>
+#include <string.h>
 #include <sstream>
 #include <stack>
 
-NameNode::NameNode() {}
+NameNode::NameNode() {
+  feature_id = -1;
+  feature_name = "";
+  state_alpha = "";
+  isEmpty = true;
+}
 
-NameNode::NameNode(std::string feature, std::string state, int i) {
+NameNode::NameNode(int id, std::string feature, std::string state, int i) : isEmpty(false) {
+  feature_id = id;
   feature_name = feature;
   state_alpha = state;
   index = i;
 }
 
 NameNode::NameNode(const NameNode & node) {
+  if (node.isEmpty)
+  {
+    feature_id = -1;
+    feature_name = "";
+    state_alpha = "";
+    isEmpty = true;
+    return;
+  } 
+
+  feature_id = node.feature_id;
   feature_name = std::move(node.feature_name);
   state_alpha = std::move(node.state_alpha);
   index = node.index;
+  isEmpty = node.isEmpty;
 }
 
 
-NameIndex::NameIndex() {}
+int NameIndex::mostSignificantBit(int x) {
+  if (x == 0)
+    return 0;
+  int mostSignificantBit = 31;
+  while ((x & (1 << mostSignificantBit)) == 0)
+    mostSignificantBit--;
+  return mostSignificantBit;
+}
+
+int NameIndex::quadraticResolution(int i) {
+  return ((i^2  + i) / 2);
+}
+ 
+NameIndex::NameIndex(int n) {
+  capacity = GoodPrimeNumbers[mostSignificantBit(n) - 1];
+  buckets = new NameNode[capacity];
+  std::fill_n(buckets, capacity, NameNode());
+}
 
 unsigned int elfHash(const std::string key) {
   /**
@@ -46,9 +82,33 @@ unsigned int elfHash(const std::string key) {
 
 void NameIndex::insert(int index, GISRecord record) {
   unsigned int keyHash = elfHash(record.feature_name + record.state_alpha);
-  NameNode node = NameNode(record.feature_name, record.state_alpha, index);
+  NameNode node = NameNode(record.feature_id, record.feature_name, record.state_alpha, index);
+  // std::cout << "inserting node:\t" << node.feature_id << "\t" << node.feature_name << "\t" << node.index << "\t" << node.isEmpty  << "\t" << node.state_alpha << std::endl;
   nameMap[keyHash] = node;
-  // nameMap[keyHash] = NameNode(record.feature_name, record.state_alpha, index);
+  int i = 0;
+  bool inserted = false;
+  int initialIndex = (keyHash + quadraticResolution(i)) % capacity;
+  while (!inserted)
+  {
+    int newIndex = (keyHash + quadraticResolution(i)) % capacity;
+    NameNode b = buckets[newIndex];
+    if (initialIndex == newIndex)
+    {
+      /* Insertion has looped indices, rehash and insert */
+      return;
+    } else if (b.isEmpty)
+    {
+      buckets[newIndex] = node;
+      inserted = true;
+    } else if (b.feature_id == record.feature_id)
+    {
+      /* Do nothing, already inserted */
+      inserted = true;
+    }
+     else {
+      i++;
+    } 
+  }
 }
 
 std::vector<int> NameIndex::search(std::string feature, std::string state) {
@@ -69,11 +129,12 @@ std::vector<int> NameIndex::search(std::string feature, std::string state) {
 
 std::string NameIndex::str()   {
   std::stringstream r;
-  r << "Format of display is \nSlot number: data record \nCurrent table size is " << nameMap.size() << "\nNumber of elements in table is " << nameMap.size() << std::endl;
+  r << "Format of display is \nSlot number: data record \nCurrent table size is " << capacity << "\nNumber of elements in table is " << nameMap.size() << std::endl;
   for (auto const& pair : nameMap) {
     // int keyHash = hasher(pair.first.feature_name + state);
     NameNode node = pair.second;
-    r << "\t" << pair.first << ": [" << node.feature_name << ":" << node.state_alpha << ", [" << node.index << "]]\n";
+    r << "\t" << pair.first << ": [" << node.feature_name << ":" << node.state_alpha << ", [" << std::to_string(node.index) << "]]\n";
+    // r << "\t" << pair.first << ": [" << node.feature_name << ":" << node.state_alpha << ", [" << std::to_string(node.index) << "]]\tID: " << node.feature_id << "\n";
   }
   return r.str();
 }
@@ -129,6 +190,7 @@ Database::Database(std::string dbFile) {
   indexCount = 1;
   std::ofstream dbStream(databaseFile);
   dbStream.close();
+  nameIndex = new NameIndex(1000);
 }
 
 void Database::insert(std::string recordLine) {
@@ -136,7 +198,7 @@ void Database::insert(std::string recordLine) {
   GISRecord record(recordLine);
   // TODO: insert to coordinate index
   // TODO: insert to name index
-  nameIndex.insert(indexCount, record);
+  nameIndex->insert(indexCount, record);
   saveToFile(recordLine);
   indexCount++;
 }
@@ -185,7 +247,7 @@ std::vector<GISRecord> Database::whatIsAt(Coordinate coord) {
 
 std::vector<std::string> Database::whatIs(std::string feature, std::string state) {
   std::cout << "What is that? " << feature << ", " << state << std::endl;
-  std::vector<int> indices = nameIndex.search(feature, state);
+  std::vector<int> indices = nameIndex->search(feature, state);
   std::vector<std::string> recordStrings;
   if (!indices.empty()) {
     std::vector<GISRecord> records = getRecords(indices);
@@ -212,5 +274,5 @@ std::vector<GISRecord> Database::whatIsIn(Region region) {
 }
 
 std::string Database::debugNameIndex() {
-  return nameIndex.str();
+  return nameIndex->str();
 }
